f11ff81ace28d5db257e89b16b0e5bb13fb73ef6
WenChenHsu <s10144244@cycu.edu.tw>
2015-08-23 06:51:30 +0800

    web 2.0

--- lovecharity/php/register.php
+++ lovecharity/php/register.php
@@ -1,14 +1,17 @@
 <?php
-include ("database.php");
 $email=$_POST['email'];
 $name=$_POST['name'];
 $password=$_POST['password'];
-if (!preg_match('/^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/', $email)) {
-	echo "<meta charset='utf-8'><script type='text/javascript'>alert('email格式錯誤');location.replace('http://lovecharity.lionfree.net');</script>";
-}else {
-	$sql="INSERT INTO `membership`(`name`, `password`, `email`) VALUES ('$name','$password','$email')";
-	$result=mysqli_query($conn,$sql) or die('insert error !');
-	header("Location:http://lovecharity.lionfree.net");
+if ($email==null) {
+	echo "email is null";
+}else{
+	echo $email;
+}
+if ($name==null) {
+	echo "name is null";
+}else{
+	echo $name;
 }
 
+echo $password;
 ?>
